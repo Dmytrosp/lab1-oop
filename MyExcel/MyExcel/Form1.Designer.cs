0996f373ce4a8c9f159219a52d9c1a4433f8c829
Dmytro Polishchuk <dmytrosp@gmail.com>
2020-11-25 19:19:21 +0200

    init lab1

---
+++ MyExcel/MyExcel/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace MyExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.buttonDeleteColumn = new System.Windows.Forms.Button();
+            this.buttonAddColumn = new System.Windows.Forms.Button();
+            this.labelColumns = new System.Windows.Forms.Label();
+            this.buttonDeleteRow = new System.Windows.Forms.Button();
+            this.labelRows = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.buttonAddRow = new System.Windows.Forms.Button();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.mainMenu = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.mainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(9, 25);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.buttonDeleteColumn);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonAddColumn);
+            this.splitContainer1.Panel1.Controls.Add(this.labelColumns);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonDeleteRow);
+            this.splitContainer1.Panel1.Controls.Add(this.labelRows);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonAddRow);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonCalculate);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(586, 336);
+            this.splitContainer1.SplitterDistance = 71;
+            this.splitContainer1.SplitterWidth = 3;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // buttonDeleteColumn
+            // 
+            this.buttonDeleteColumn.Location = new System.Drawing.Point(225, 28);
+            this.buttonDeleteColumn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonDeleteColumn.Name = "buttonDeleteColumn";
+            this.buttonDeleteColumn.Size = new System.Drawing.Size(22, 19);
+            this.buttonDeleteColumn.TabIndex = 2;
+            this.buttonDeleteColumn.Text = "-";
+            this.buttonDeleteColumn.UseVisualStyleBackColor = true;
+            this.buttonDeleteColumn.Click += new System.EventHandler(this.buttonDeleteColumn_Click);
+            // 
+            // buttonAddColumn
+            // 
+            this.buttonAddColumn.Location = new System.Drawing.Point(188, 28);
+            this.buttonAddColumn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonAddColumn.Name = "buttonAddColumn";
+            this.buttonAddColumn.Size = new System.Drawing.Size(22, 19);
+            this.buttonAddColumn.TabIndex = 4;
+            this.buttonAddColumn.Text = "+";
+            this.buttonAddColumn.UseVisualStyleBackColor = true;
+            this.buttonAddColumn.Click += new System.EventHandler(this.buttonAddColumn_Click);
+            // 
+            // labelColumns
+            // 
+            this.labelColumns.AutoSize = true;
+            this.labelColumns.Location = new System.Drawing.Point(192, 11);
+            this.labelColumns.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelColumns.Name = "labelColumns";
+            this.labelColumns.Size = new System.Drawing.Size(47, 13);
+            this.labelColumns.TabIndex = 3;
+            this.labelColumns.Text = "Columns";
+            // 
+            // buttonDeleteRow
+            // 
+            this.buttonDeleteRow.Location = new System.Drawing.Point(150, 28);
+            this.buttonDeleteRow.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonDeleteRow.Name = "buttonDeleteRow";
+            this.buttonDeleteRow.Size = new System.Drawing.Size(22, 19);
+            this.buttonDeleteRow.TabIndex = 3;
+            this.buttonDeleteRow.Text = "-";
+            this.buttonDeleteRow.UseVisualStyleBackColor = true;
+            this.buttonDeleteRow.Click += new System.EventHandler(this.buttonDeleteRow_Click);
+            // 
+            // labelRows
+            // 
+            this.labelRows.AutoSize = true;
+            this.labelRows.Location = new System.Drawing.Point(125, 11);
+            this.labelRows.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelRows.Name = "labelRows";
+            this.labelRows.Size = new System.Drawing.Size(34, 13);
+            this.labelRows.TabIndex = 2;
+            this.labelRows.Text = "Rows";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(282, 28);
+            this.textBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(183, 20);
+            this.textBox.TabIndex = 1;
+            // 
+            // buttonAddRow
+            // 
+            this.buttonAddRow.Location = new System.Drawing.Point(112, 28);
+            this.buttonAddRow.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonAddRow.Name = "buttonAddRow";
+            this.buttonAddRow.Size = new System.Drawing.Size(22, 19);
+            this.buttonAddRow.TabIndex = 1;
+            this.buttonAddRow.Text = "+";
+            this.buttonAddRow.UseVisualStyleBackColor = true;
+            this.buttonAddRow.Click += new System.EventHandler(this.buttonAddRow_Click);
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(481, 22);
+            this.buttonCalculate.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(90, 24);
+            this.buttonCalculate.TabIndex = 0;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 2);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(580, 271);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuFile,
+            this.toolStripMenuHelp});
+            this.mainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.mainMenu.Size = new System.Drawing.Size(600, 24);
+            this.mainMenu.TabIndex = 1;
+            this.mainMenu.Text = "menuStrip1";
+            // 
+            // toolStripMenuFile
+            // 
+            this.toolStripMenuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuOpen,
+            this.toolStripMenuSave});
+            this.toolStripMenuFile.Name = "toolStripMenuFile";
+            this.toolStripMenuFile.Size = new System.Drawing.Size(37, 20);
+            this.toolStripMenuFile.Text = "File";
+            // 
+            // toolStripMenuOpen
+            // 
+            this.toolStripMenuOpen.Name = "toolStripMenuOpen";
+            this.toolStripMenuOpen.Size = new System.Drawing.Size(103, 22);
+            this.toolStripMenuOpen.Text = "Open";
+            this.toolStripMenuOpen.Click += new System.EventHandler(this.toolStripMenuOpen_Click);
+            // 
+            // toolStripMenuSave
+            // 
+            this.toolStripMenuSave.Name = "toolStripMenuSave";
+            this.toolStripMenuSave.Size = new System.Drawing.Size(103, 22);
+            this.toolStripMenuSave.Text = "Save";
+            this.toolStripMenuSave.Click += new System.EventHandler(this.toolStripMenuSave_Click);
+            // 
+            // toolStripMenuHelp
+            // 
+            this.toolStripMenuHelp.Name = "toolStripMenuHelp";
+            this.toolStripMenuHelp.Size = new System.Drawing.Size(44, 20);
+            this.toolStripMenuHelp.Text = "Help";
+            this.toolStripMenuHelp.Click += new System.EventHandler(this.toolStripMenuHelp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.mainMenu);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "MyExcel ";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.mainMenu.ResumeLayout(false);
+            this.mainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label labelColumns;
+        private System.Windows.Forms.Label labelRows;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuFile;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuOpen;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuSave;
+        private System.Windows.Forms.Button buttonAddRow;
+        private System.Windows.Forms.Button buttonAddColumn;
+        private System.Windows.Forms.Button buttonDeleteRow;
+        private System.Windows.Forms.Button buttonDeleteColumn;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuHelp;
+    }
+}
+
